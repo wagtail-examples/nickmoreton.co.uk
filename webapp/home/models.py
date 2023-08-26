3e5dc9af9761f96f2bd37caca6027cbf199c2330
Nick Moreton <nickmoreton@me.com>
2023-08-26 15:05:47 +0100

    Show latest on home page

--- webapp/home/models.py
+++ webapp/home/models.py
@@ -3,6 +3,7 @@ from wagtail.fields import StreamField
 from wagtail.models import Page
 
 from webapp.pages.blocks import BaseBlocks
+from webapp.pages.models import ArticlePage, PackagePage
 
 
 class HomePage(Page):
@@ -16,3 +17,15 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("body"),
     ]
+
+    def latest_article(self):
+        return ArticlePage.objects.live().order_by("-date").first()
+
+    def latest_package(self):
+        return PackagePage.objects.live().order_by("-date").first()
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["latest_article"] = self.latest_article()
+        context["latest_package"] = self.latest_package()
+        return context
