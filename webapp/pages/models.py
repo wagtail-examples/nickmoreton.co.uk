c22463b99304e4a0b751fd918b4775175b6c3322
Nick Moreton <nickmoreton@me.com>
2023-08-26 14:52:05 +0100

    Initial packages pages

--- webapp/pages/models.py
+++ webapp/pages/models.py
@@ -53,3 +53,47 @@ class ArticlePage(BasePage):
         context = super().get_context(request)
         context["latest_articles"] = self.get_latest_articles()
         return context
+
+
+class PackageIndexPage(BasePage):
+    subpage_types = ["pages.PackagePage"]
+    body = StreamField(
+        BaseBlocks,
+        blank=True,
+        use_json_field=True,
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["packages"] = (
+            PackagePage.objects.live().descendant_of(self).order_by("-date")
+        )
+        return context
+
+
+class PackagePage(BasePage):
+    parent_page_types = ["pages.PackageIndexPage"]
+    body = StreamField(
+        BaseBlocks,
+        blank=True,
+        use_json_field=True,
+    )
+    date = models.DateField("Package published date")
+    excerpt = models.CharField(max_length=255, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+        FieldPanel("date"),
+        FieldPanel("excerpt"),
+    ]
+
+    def get_latest_packages(self):
+        return PackagePage.objects.live().order_by("-date")[:3]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["latest_packages"] = self.get_latest_packages()
+        return context
