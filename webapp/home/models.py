76e4b6d9418129d8d48532344f6a97aca37eb4fd
Nick Moreton <nickmoreton@me.com>
2023-08-26 14:47:44 +0100

    Adjust home page

--- webapp/home/models.py
+++ webapp/home/models.py
@@ -1,5 +1,18 @@
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
 from wagtail.models import Page
+
+from webapp.pages.blocks import BaseBlocks
 
 
 class HomePage(Page):
     max_count = 1
+    body = StreamField(
+        BaseBlocks,
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
