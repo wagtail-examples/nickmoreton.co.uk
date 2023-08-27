085708eb44384cd0014dc51417437c5fce7dd6d3
Nick Moreton <nickmoreton@me.com>
2023-08-27 14:17:05 +0100

    Add chapters to articles

--- webapp/pages/models.py
+++ webapp/pages/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import StreamField
 from wagtail.models import Page
 
 from webapp.pages.blocks import BaseBlocks
 
 
+# ABSTRACT MODELS
 class BasePage(Page):
     class Meta:
         abstract = True
 
 
+# ARTICLE MODELS
 class ArticleIndexPage(BasePage):
     subpage_types = ["pages.ArticlePage"]
     body = StreamField(
@@ -30,21 +32,16 @@ class ArticleIndexPage(BasePage):
         return context
 
 
-class ArticlePage(BasePage):
-    parent_page_types = ["pages.ArticleIndexPage"]
+class ArticlePageBase(BasePage):
     body = StreamField(
         BaseBlocks,
         blank=True,
         use_json_field=True,
     )
-    date = models.DateField("Article published date")
     excerpt = models.CharField(max_length=255, blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel("body"),
-        FieldPanel("date"),
-        FieldPanel("excerpt"),
-    ]
+    class Meta:
+        abstract = True
 
     def get_latest_articles(self):
         articles = ArticlePage.objects.live().order_by("-date").exclude(id=self.id)
@@ -55,7 +52,63 @@ class ArticlePage(BasePage):
         context["latest_articles"] = self.get_latest_articles()
         return context
 
+    def get_chapters(self):
+        chapters = self.get_children().live().type(ArticleChapterPage)
+        return chapters
 
+    base_panels_top = [
+        FieldPanel("excerpt"),
+    ]
+
+    base_panels_bottom = [
+        FieldPanel("body"),
+    ]
+
+
+class ArticlePage(ArticlePageBase):
+    subpage_types = ["pages.ArticleChapterPage"]
+    date = models.DateField("Article published date")
+
+    panels_top = [
+        MultiFieldPanel(
+            ArticlePageBase.base_panels_top
+            + [
+                FieldPanel("date"),
+            ],
+            heading="Article information",
+        )
+    ]
+
+    panels_bottom = [
+        FieldPanel("body"),
+    ]
+
+    content_panels = Page.content_panels + panels_top + panels_bottom
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["chapters"] = self.get_children().live().type(ArticleChapterPage)
+        return context
+
+
+class ArticleChapterPage(ArticlePageBase):
+    subpage_types = []
+
+    panels_bottom = ArticlePageBase.base_panels_bottom
+
+    content_panels = Page.content_panels + panels_bottom
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["article"] = self.get_parent()
+        context["chapters"] = ArticleChapterPage.objects.live().sibling_of(
+            self, inclusive=True
+        )
+        context["current_chapter"] = self
+        return context
+
+
+# PACKAGE MODELS
 class PackageIndexPage(BasePage):
     subpage_types = ["pages.PackagePage"]
     body = StreamField(
