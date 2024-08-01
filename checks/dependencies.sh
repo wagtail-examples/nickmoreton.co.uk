7173139f5849770c719c8b6282ca8399fb0ce1b0
nickmoreton <nick.moreton@torchbox.com>
2024-08-01 22:27:48 +0100

    Update dependencies check

--- checks/dependencies.sh
+++ checks/dependencies.sh
@@ -7,7 +7,7 @@ requirements=$(cat requirements.txt)
 installed_requirements=$(docker-compose exec web bash -c "pip freeze")
 
 # Compare the two outputs
-if [ "$requirements" == "$pipenv_requirements" ]; then
+if [ "$requirements" == "$installed_requirements" ]; then
   echo "Requirements are correct"
 else
   echo "Requirements are different, please run pipenv requirements > requirements.txt and stage the changes"
