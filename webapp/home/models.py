22b2624a70804a7b2dff9389bb6cf0e236e2ff31
nickmoreton <nickmoreton@me.com>
2025-03-14 16:51:28 +0000

    Revert "Test for missing migrations"
    This reverts commit 948316be88b7040b0a32871fcc8e06fad5eae08d.

--- webapp/home/models.py
+++ webapp/home/models.py
@@ -13,11 +13,6 @@ class HomePage(Page):
         blank=True,
         use_json_field=True,
     )
-    fake_field = StreamField(
-        BaseBlocks,
-        blank=True,
-        use_json_field=True,
-    )
 
     content_panels = Page.content_panels + [
         FieldPanel("body"),
